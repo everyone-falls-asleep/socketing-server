776dfa8fb7e8576687972546c0812907316da1d0
jee-in <chris309804@gmail.com>
2024-12-03 13:13:23 +0900

    fix: Apply prettier format and eslint

--- src/reservations/dto/request/create-order-request.dto.ts
+++ src/reservations/dto/request/create-order-request.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { IsArray, IsString } from 'class-validator';
 
 export class CreateOrderRequestDto {
   @ApiProperty({
